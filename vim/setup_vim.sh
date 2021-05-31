4dd718b7401303a582317709b9c6677a54960049
Chandradeep Pokhariya <chinu.deep100@gmail.com>
2021-05-31 08:03:48 +0530

    modified

--- vim/setup_vim.sh
+++ vim/setup_vim.sh
@@ -2,8 +2,12 @@ git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 cp ./.vimrc ~/
-cp ./basic.vim ~/
+cp ./basic.vim ~/.vim_runtime/vimrcs/
 
-mkdir ~/.vim
+if [ ! -d ~/.vim ]
+then
+    mkdir ~/.vim
+fi
+
 cp -r ./colors ~/.vim/
-
+echo "------- COMPLETED --------"
